1cb882957f687cfebd6edfaa4b24159150a94203
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-04-06 18:13:50 +0200

    .

--- SIFT/scripts/outdoor_ada.sh
+++ SIFT/scripts/outdoor_ada.sh
@@ -22,7 +22,8 @@ batch_size=1  # per gpu
 
 CUDA_VISIBLE_DEVICES=1 python3 -u ./test_lightning.py \
     ${data_cfg_path} \
-    --ckpt_path=${ckpt_path} \
+    --ckpt_path=${pretrained_model_path} \
+    --model_type=${model_type} \
     --dump_dir=${dump_dir} \
     --gpus=${n_gpus_per_node} --num_nodes=${n_nodes} --accelerator="cuda" \
     --batch_size=${batch_size} --num_workers=${torch_num_workers}\
