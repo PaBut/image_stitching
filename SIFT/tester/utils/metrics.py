0d507a403975b3a69a2a859207a92f23c14d03bc
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-04-06 19:22:01 +0200

    .

--- SIFT/tester/utils/metrics.py
+++ SIFT/tester/utils/metrics.py
@@ -127,6 +127,7 @@ def compute_symmetrical_epipolar_errors(data):
     logger.debug(f"m_bids: {Tx.size(0)}")
     for bs in range(Tx.size(0)):
         mask = m_bids == bs
+        logger.debug(f"mask: {mask}, pts0: {pts0[mask].shape}, pts1: {pts1[mask].shape}")
         epi_errs.append(
             symmetric_epipolar_distance(
                 pts0[mask], pts1[mask], E_mat[bs], data["K0"][bs], data["K1"][bs]
