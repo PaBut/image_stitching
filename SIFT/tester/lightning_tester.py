c3883a9c40e6fd91f26c9f3de4cc1e994cf1ea74
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-04-06 18:59:54 +0200

    fixed

--- SIFT/tester/lightning_tester.py
+++ SIFT/tester/lightning_tester.py
@@ -86,8 +86,8 @@ class PL_Tester(pl.LightningModule):
         img1 = batch["image1"].permute(0, 2, 3, 1).cpu().numpy()
         logger.info(f"img0 shape: {img0.shape}, img1 shape: {img1.shape}")
         k1, k2 = self.matcher.find_matches(img0[0], img1[0])
-        batch["mkpts0_f"] = torch.from_numpy(k1).cuda().unsqueeze(0)
-        batch["mkpts1_f"] = torch.from_numpy(k2).cuda().unsqueeze(0)
+        batch["mkpts0_f"] = torch.from_numpy(k1).cuda().float().unsqueeze(0)
+        batch["mkpts1_f"] = torch.from_numpy(k2).cuda().float().unsqueeze(0)
         batch["m_bids"] = [0]
 
         ret_dict, rel_pair_names = self._compute_metrics(batch)
