ac8c616675f60b8acebea3fefa60bd39bc6c016d
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-04-10 15:31:31 +0200

    changed script

--- SIFT/scripts/walkdepth.sh
+++ SIFT/scripts/walkdepth.sh
@@ -10,13 +10,13 @@ cd $PROJECT_DIR
 model_type=$1
 pretrained_model_path=$2
 
-data_cfg_path="tester/configs/walkdepth_test_config.py"
+data_cfg_path="tester/configs/walkdepth2_test_config.py"
 
 dump_dir="dump/loftr_ds_outdoor"
 profiler_name="inference"
 n_nodes=1  # manually keep this the same with --nodes
 n_gpus_per_node=1 # -1
-torch_num_workers=4 # 4
+torch_num_workers=8 # 4
 batch_size=1  # per gpu
 
 CUDA_VISIBLE_DEVICES=0 python3 -u ./test_lightning.py \
