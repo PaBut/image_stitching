acfd4ed2e0e75d5861bb6315877f49f14fb7af14
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-05-07 16:08:39 +0200

    renamed directories

--- scripts/walkdepth_plot.sh
+++ scripts/walkdepth_plot.sh
@@ -14,13 +14,6 @@ figure_path=$4
 
 data_cfg_path="tester/configs/walkdepth_test_config.py"
 
-dump_dir="dump/loftr_ds_outdoor"
-profiler_name="inference"
-n_nodes=1  # manually keep this the same with --nodes
-n_gpus_per_node=1 # -1
-torch_num_workers=8 # 4
-batch_size=1  # per gpu
-
 CUDA_VISIBLE_DEVICES=0 python3 -u ./test_plot.py \
     ${data_cfg_path} \
     --ckpt_path=${pretrained_model_path} \
