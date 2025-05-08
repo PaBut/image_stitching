9add5667a9d1c1e3e9b3029265598409feb06ca4
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-05-08 15:18:03 +0200

    fixed scripts

--- scripts/walkdepth_plot.sh
+++ scripts/walkdepth_plot.sh
@@ -14,7 +14,7 @@ figure_path=$4
 
 data_cfg_path="tester/configs/walkdepth_test_config.py"
 
-CUDA_VISIBLE_DEVICES=0 python3 -u ./test_plot.py \
+CUDA_VISIBLE_DEVICES=0 python3 -u ./testing/test_plot.py \
     ${data_cfg_path} \
     --ckpt_path=${pretrained_model_path} \
     --model_type=${model_type} \
