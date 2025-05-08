0c43f83b251765ca6181b2710575bea976620b03
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-05-08 16:08:30 +0200

    prepared setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,12 @@
-gdown "https://drive.google.com/uc?id=1OaG0ayEwRPhKVV_OwQwvwHDFHC26iv30" -O "./pipeline/Modules/tools/UDIS2/Composition/model" # UDIS++ Composition weights
-gdown "https://drive.google.com/uc?id=1GBwB0y3tUUsOYHErSqxDxoC_Om3BJUEt" -O "./pipeline/Modules/tools/UDIS2/Warp/model"  # UDIS++ Warp weights
-gdown "https://drive.google.com/uc?id=1M-VD35-qdB5Iw-AtbDBCKC7hPolFW9UY" -O "./pipeline/Modules/tools/LoFTR/weights"  # LoFTR weights
-gdown "https://drive.google.com/uc?id=1ploO_h0eg7G6WW1xfloDgHHWniO_2Nz7" -O "./pipeline/Modules/tools/AdaMatcherUtils/weights"  # AdaMacther weights
+SCRIPTPATH=$(dirname $(readlink -f "$0"))
+PROJECT_DIR="${SCRIPTPATH}/../"
+
+cd $PROJECT_DIR
+
+gdown "https://drive.google.com/uc?id=1OaG0ayEwRPhKVV_OwQwvwHDFHC26iv30" -O "./pipeline/Modules/tools/UDIS2/Composition/model/" # UDIS++ Composition weights
+gdown "https://drive.google.com/uc?id=1GBwB0y3tUUsOYHErSqxDxoC_Om3BJUEt" -O "./pipeline/Modules/tools/UDIS2/Warp/model/"  # UDIS++ Warp weights
+gdown "https://drive.google.com/uc?id=1M-VD35-qdB5Iw-AtbDBCKC7hPolFW9UY" -O "./pipeline/Modules/tools/LoFTR/weights/"  # LoFTR weights
+gdown "https://drive.google.com/uc?id=1ploO_h0eg7G6WW1xfloDgHHWniO_2Nz7" -O "./pipeline/Modules/tools/AdaMatcherUtils/weights/"  # AdaMacther weights
+
+conda env create -f environment.yml
+conda activate image_stitching
