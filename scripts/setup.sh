2b838dad7a4b188b55634a321602043e8add3178
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-05-08 12:23:09 +0200

    added environemtn setup, reuiqrements and script for weights downloading

---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+gdown "https://drive.google.com/uc?id=1OaG0ayEwRPhKVV_OwQwvwHDFHC26iv30" -O "./pipeline/Modules/tools/UDIS2/Composition/model" # UDIS++ Composition weights
+gdown "https://drive.google.com/uc?id=1GBwB0y3tUUsOYHErSqxDxoC_Om3BJUEt" -O "./pipeline/Modules/tools/UDIS2/Warp/model"  # UDIS++ Warp weights
+gdown "https://drive.google.com/uc?id=1M-VD35-qdB5Iw-AtbDBCKC7hPolFW9UY" -O "./pipeline/Modules/tools/LoFTR/weights"  # LoFTR weights
+gdown "https://drive.google.com/uc?id=1ploO_h0eg7G6WW1xfloDgHHWniO_2Nz7" -O "./pipeline/Modules/tools/AdaMatcherUtils/weights"  # AdaMacther weights
