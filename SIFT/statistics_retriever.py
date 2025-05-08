6b618ecb801a90bc4a37cee599d583119ba37991
Pavlo Butenko <pabutik@zia1.metacentrum.cz>
2025-05-08 12:34:34 +0200

    Added server changes

--- SIFT/statistics_retriever.py
+++ SIFT/statistics_retriever.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 
-def process_file(filename, base_path):
+def process_file(filename, base_path, overlapping_ratio):
     img_count = 0
     pair_count = 0
     valid_pair_count = 0
@@ -21,12 +21,12 @@ def process_file(filename, base_path):
 
                 img_count += len(images)
                 pair_count += len(pairs)
-                valid_pair_count += len([pair for pair in pairs if pair[1] > 0.15])
+                valid_pair_count += len([pair for pair in pairs if pair[1] > overlapping_ratio])
 
                 print(f"Processing {npz_path}:")
                 print(f"  Image count: {len(images)}")
                 print(f"  Pair count: {len(pairs)}")
-                print(f"  Valid pair count: {len([pair for pair in pairs if pair[1] > 0.15])}")
+                print(f"  Valid pair count: {len([pair for pair in pairs if pair[1] > overlapping_ratio])}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -35,7 +35,7 @@ def process_file(filename, base_path):
     print(f"Valid pair count: {valid_pair_count}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <filename.txt> <base_path>")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <filename.txt> <base_path> <overlapping_ratio>")
     else:
-        process_file(sys.argv[1], sys.argv[2])
+        process_file(sys.argv[1], sys.argv[2], float(sys.argv[3]))
