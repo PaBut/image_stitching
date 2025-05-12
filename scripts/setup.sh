ee9e85a5f5e9446e5209480a8ae9e447b5b9cbb5
Pavlo Butenko <pavlo.butenko@thermofisher.com>
2025-05-12 16:22:59 +0200

    fixed dependencies and setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ PROJECT_DIR="${SCRIPTPATH}/../"
 
 cd $PROJECT_DIR
 
-gdown "https://drive.google.com/uc?id=1OaG0ayEwRPhKVV_OwQwvwHDFHC26iv30" -O "./models/UDIS2/Composition/model/" # UDIS++ Composition weights
-gdown "https://drive.google.com/uc?id=1GBwB0y3tUUsOYHErSqxDxoC_Om3BJUEt" -O "./models/UDIS2/Warp/model/"  # UDIS++ Warp weights
-gdown "https://drive.google.com/uc?id=1M-VD35-qdB5Iw-AtbDBCKC7hPolFW9UY" -O "./models/LoFTR/weights/"  # LoFTR weights
-gdown "https://drive.google.com/uc?id=1ploO_h0eg7G6WW1xfloDgHHWniO_2Nz7" -O "./AdaMatcher/weights/"  # AdaMatcher weights
+python3 -m gdown "https://drive.google.com/uc?id=1OaG0ayEwRPhKVV_OwQwvwHDFHC26iv30" -O "./models/UDIS2/Composition/model/" # UDIS++ Composition weights
+python3 -m gdown "https://drive.google.com/uc?id=1GBwB0y3tUUsOYHErSqxDxoC_Om3BJUEt" -O "./models/UDIS2/Warp/model/"  # UDIS++ Warp weights
+python3 -m gdown "https://drive.google.com/uc?id=1M-VD35-qdB5Iw-AtbDBCKC7hPolFW9UY" -O "./models/LoFTR/weights/"  # LoFTR weights
+python3 -m gdown "https://drive.google.com/uc?id=1ploO_h0eg7G6WW1xfloDgHHWniO_2Nz7" -O "./AdaMatcher/weights/"  # AdaMatcher weights
